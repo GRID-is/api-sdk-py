ef9eefcadb1f7979f157b069a1db99a483c28f87
atlipall <atlipall@gmail.com>
2025-04-02 15:34:37 +0000

    Add goal seek example

---
+++ examples/goal_seek.py
@@ -0,0 +1,68 @@
+# /// script
+# requires-python = ">=3.9"
+# dependencies = [
+#     "fastapi[standard]",
+#     "grid-api",
+#     "uvicorn",
+# ]
+# ///
+import uvicorn
+
+from fastapi import FastAPI
+from grid_api import Grid, APIConnectionError, APIStatusError, RateLimitError
+
+app = FastAPI()
+
+@app.get("/seek_interests")
+async def seek_interests(target_amount: float):
+    client = Grid()
+
+    try:
+        response = client.workbooks.query(
+            id="353ed262-930c-4872-a585-fe82203066ae",
+            goal_seek={
+                "targetCell": "Sheet1!C7",
+                "targetValue": target_amount,
+                "controlCell": "Sheet1!C5",
+            },
+            read=[]
+        )
+    except APIConnectionError as e:
+        return {"error": "The server could not be reached."}
+    except RateLimitError as e:
+        return {"error", "A 429 status code was received; we should back off a bit."}
+    except APIStatusError as e:
+        return {"error": e.message}
+
+    solution = response.goalSeek["solution"]
+
+    return {"interest_rate": solution}
+
+
+@app.get("/seek_years")
+async def seek_years(target_amount: float):
+    client = Grid()
+
+    try:
+        response = client.workbooks.query(
+            id="353ed262-930c-4872-a585-fe82203066ae",
+            goal_seek={
+                "targetCell": "Sheet1!C7",
+                "targetValue": target_amount,
+                "controlCell": "Sheet1!C4",
+            },
+            read=[]
+        )
+    except APIConnectionError as e:
+        return {"error": "The server could not be reached."}
+    except RateLimitError as e:
+        return {"error", "A 429 status code was received; we should back off a bit."}
+    except APIStatusError as e:
+        return {"error": e.message}
+
+    solution = response.goalSeek["solution"]
+
+    return {"years": solution}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
